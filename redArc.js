6b37420237afbbf574350a7f34fb15c203af45a9
SN4T14 <SN4T14@users.noreply.github.com>
2015-06-10 21:12:40 +0000

    Added handling for imgur gifs, fixed indentation in linkHandler.js, fixed bug when adding new users to the list of users to archive, fixed package.json

--- redArc.js
+++ redArc.js
@@ -40,7 +40,10 @@ post_stream.on('new', function(posts) {
 		var post = posts[i].data; // Damn it, JS, implement proper foreach!
 		var save = false;
 
-		if (subredditsToArchive.indexOf(post.subreddit) !== -1) {
+
+		if (usersToArchive.indexOf(post.author) !== -1) {
+			save = true;
+		} else if (subredditsToArchive.indexOf(post.subreddit) !== -1) { // else if to avoid adding users that already exist in usersToArchive
 			addUser(post.author);
 			save = true;
 		}
